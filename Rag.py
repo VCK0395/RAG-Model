ac5490b962a43c02fa474b4d4d4f96e32642de84
Victor Kalu <87344807+VCK0395@users.noreply.github.com>
2025-10-01 20:39:46 -0500

    Update Rag.py

--- Rag.py
+++ Rag.py
@@ -1,1 +1,98 @@
 
+import os
+import glob
+from dotenv import load_dotenv
+import gradio as gr
+# imports for langchain
+
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.schema import Document
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_chroma import Chroma
+import numpy as np
+from sklearn.manifold import TSNE
+import plotly.graph_objects as go
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+# price is a factor for our company, so we're going to use a low cost model
+
+MODEL = "gemini-2.5-flash"
+db_name = "vector_db"
+# Load environment variables in a file called .env
+
+load_dotenv(override=True)
+os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY', 'your-key-if-not-using-env')
+# Read in documents using LangChain's loaders
+# Take everything in all the sub-folders of our knowledgebase
+
+folders = glob.glob("knowledge-base/*")
+
+# With thanks to CG and Jon R, students on the course, for this fix needed for some users 
+text_loader_kwargs = {'encoding': 'utf-8'}
+# If that doesn't work, some Windows users might need to uncomment the next line instead
+# text_loader_kwargs={'autodetect_encoding': True}
+
+documents = []
+for folder in folders:
+    doc_type = os.path.basename(folder)
+    loader = DirectoryLoader(folder, glob="**/*.md", loader_cls=TextLoader, loader_kwargs=text_loader_kwargs)
+    folder_docs = loader.load()
+    for doc in folder_docs:
+        doc.metadata["doc_type"] = doc_type
+        documents.append(doc)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+chunks = text_splitter.split_documents(documents)
+
+len(chunks)
+
+doc_types = set(chunk.metadata['doc_type'] for chunk in chunks)
+print(f"Document types found: {', '.join(doc_types)}")
+
+from langchain.embeddings import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# Delete if already exists
+
+if os.path.exists(db_name):
+    Chroma(persist_directory=db_name, embedding_function=embeddings).delete_collection()
+
+
+# Create vectorstore
+
+vectorstore = Chroma.from_documents(documents=chunks, embedding=embeddings, persist_directory=db_name)
+print(f"Vectorstore created with {vectorstore._collection.count()} documents")
+
+# Get one vector and find how many dimensions it has
+
+collection = vectorstore._collection
+sample_embedding = collection.get(limit=1, include=["embeddings"])["embeddings"][0]
+dimensions = len(sample_embedding)
+print(f"The vectors have {dimensions:,} dimensions")
+
+# create a new Chat with OpenAI
+llm = ChatGoogleGenerativeAI(
+    temperature=0.7, 
+    model=MODEL
+)
+
+# set up the conversation memory for the chat
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+# the retriever is an abstraction over the VectorStore that will be used during RAG
+retriever = vectorstore.as_retriever()
+
+# putting it together: set up the conversation chain with the GPT 4o-mini LLM, the vector store and memory
+conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory)
+
+query = "Can you describe Insurellm in a few sentences"
+result = conversation_chain.invoke({"question":query})
+print(result["answer"])
+
+# set up a new conversation memory for the chat
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+# putting it together: set up the conversation chain with the GPT 4o-mini LLM, the vector store and memory
+conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory)
+
